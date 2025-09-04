70ee2a1b619c2312b7d833b3a7ee5a3d69b45b9c
Nemairy <anemairy9090@gmail.com>
2025-09-04 22:11:43 +0200

    Feature/ added the rental table

---
+++ src/main/java/com/openclassrooms/Chatop/DTOs/RentalCreUpDto.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.Chatop.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RentalCreUpDto {
+	
+	private String name;
+	private Integer surface;
+	private Integer price;
+	private String picture;
+	private String description;
+}
---
+++ src/main/java/com/openclassrooms/Chatop/DTOs/RentalDto.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.Chatop.DTOs;
+
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RentalDto {
+	
+	private Long id;
+	private String name;
+	private Integer surface;
+	private Integer price;
+	private String picture;
+	private String description;
+	
+	@JsonProperty("owner_id")
+	private Long ownerId;
+	
+	@JsonProperty("created-at")
+	private Instant createdAt;
+	
+	@JsonProperty("updated_at")
+	private Instant updatedAt;
+
+}
