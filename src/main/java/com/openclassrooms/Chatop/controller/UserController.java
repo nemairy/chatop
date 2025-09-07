c8ebd2e057a3f11b85c4bab8ee880c039c70c756
Nemairy <anemairy9090@gmail.com>
2025-09-07 08:08:09 +0200

    Added public user endpoint

---
+++ src/main/java/com/openclassrooms/Chatop/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.Chatop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.Chatop.DTOs.MeResponse;
+import com.openclassrooms.Chatop.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/user/{id}")
+	public MeResponse getUser(@PathVariable Long id) {
+		return userService.getPublicUser(id);
+	}
+	
+
+}
---
+++ src/main/java/com/openclassrooms/Chatop/service/UserService.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.Chatop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.openclassrooms.Chatop.DTOs.MeResponse;
+import com.openclassrooms.Chatop.model.UserN;
+import com.openclassrooms.Chatop.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public MeResponse getPublicUser(Long id) {
+		UserN user = userRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"+ id));
+		
+		return new MeResponse(
+				user.getId(),
+				user.getName(),
+				user.getEmail(),
+				user.getCreatedAt(),
+				user.getUpdatedAt());
+	}
+}
